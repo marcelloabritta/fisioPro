6e6af96bfc767344b075a8c418209e9766a4d23a
marcelloabritta <abrittamarcello@gmail.com>
2025-11-05 19:42:25 -0300

    createForm

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -1,13 +1,136 @@
-"use client"
-import { useProfileForm } from "./profile-form"
+"use client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useProfileForm } from "./profile-form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+import imgTest from "../../../../../../public/foto1.png";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export function ProfileContent() {
+  const form = useProfileForm();
 
-    const form = useProfileForm()
+  return (
+    <div className="mx-auto">
+      <Form {...form}>
+        <form>
+          <Card>
+            <CardHeader>
+              <CardTitle>Minha Clínica</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-6">
+              <div className="flex justify-center">
+                <div className="bg-gray-200 relative h-40 w-40 rounded-full overflow-hidden">
+                  <Image
+                    src={imgTest}
+                    alt="Foto da clínica"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              </div>
 
-    return(
-        <div>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Nome da Clinica
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Digite o nome da clinica..."
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
 
-        </div>
-    )
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Endereço completo
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Digite o endereço da clinica..."
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Número de telefone
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Digite o telefone da clinica..."
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="status"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Status da Clínica
+                      </FormLabel>
+                      <FormControl>
+                        <Select 
+                        onValueChange={field.onChange} 
+                        defaultValue={field.value? "active" : "inactive"}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder="Selecione o status da clínica" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="active">
+                              ATIVO (clínica aberta)
+                            </SelectItem>
+                            <SelectItem value="inactive">
+                              INATIVO (clínica fechada)
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
+    </div>
+  );
 }
